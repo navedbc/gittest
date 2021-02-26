66259047f6f9a377dd9756392046f638db3a9e5d
navedbc <navedbc@gmail.com>
2021-02-26 11:24:35 -0800

    added display level changes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
 
 
+    private void display(String str){
+        System.out.println(
+                str
+        );
+    }
+
+
     public static void main(String[] args) {
         System.out.println("this is the first proj");
         System.out.println("display changes");
+        System.out.println("this is the third changes in this branch");
     }
 }
